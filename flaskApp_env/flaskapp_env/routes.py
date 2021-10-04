f6c8caf4d6354a0b25f0459aa33df3430dccd299
Yi-Jung <emilyichan3@gmail.com>
2021-10-04 13:53:50 +0100

    User Account and Profile Picture

--- flaskApp_env/flaskapp_env/routes.py
+++ flaskApp_env/flaskapp_env/routes.py
@@ -1,8 +1,11 @@
+import os
+import secrets
+from PIL import Image #pip install Pillow
 from flask import flash, redirect, render_template, url_for, request
-from flaskapp_env import app, db, bcrypt
-from flaskapp_env.forms import RegistrationForm, LoginForm
-from flaskapp_env.modules import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+from flaskapp_env import app, db, bcrypt
+from flaskapp_env.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskapp_env.modules import User, Post
 
 recipes = [
     {
@@ -59,14 +62,43 @@ def login():
             flash('Login Unsuccessful. Pleach check email and password!','danger')
     return render_template('login.html', title='Login', form=form)
 
-
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    #使用 splitext區分檔案名稱和類型，最前面的_代表 "忽略回傳的第一個值"
+    _, f_ext = os.path.splitext(form_picture.filename)
+    #重新命名檔名為8字元的亂數，但維持相同的副檔名
+    picture_fn = random_hex + f_ext 
+    #新的檔案路徑
+    picture_path = os.path.join(app.root_path, 'static\profile_pics', picture_fn)
+    #re-size the picture (from Pillow package)
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
 
-@app.route('/account')
+    return picture_fn
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        # form.XXXX 指label，內容的話要.data
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
