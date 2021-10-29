359143e2c3e2f213fe67360c9315847ef99c27a7
Yi-Jung <emilyichan3@gmail.com>
2021-10-29 10:37:55 +0100

    Mail html

--- flaskApp_env/App/flaskapp_env/users/utils.py
+++ flaskApp_env/App/flaskapp_env/users/utils.py
@@ -2,7 +2,7 @@ import os
 import secrets
 from PIL import Image #pip install Pillow
 from flask_mail import Message
-from flask import url_for, current_app
+from flask import url_for, current_app, render_template
 from flaskapp_env import mail
 
 def save_picture(form_picture):
@@ -23,21 +23,29 @@ def save_picture(form_picture):
 
 def send_reset_email(user):
     token = user.get_reset_token()
+
     msg = Message('Password Reset Request', 
                 sender='noreply@demo.com', 
-                recipients=[user.email])
-    msg.body = f'''To reset your password, click the following link:
-{url_for('users.reset_token',token=token, _external=True)}
+                cc = [user.email],
+                reply_to = 'emilyichan3@gmail.com')
+#     msg.body = f'''To reset your password, click the following link:
+# {url_for('users.reset_token',token=token, _external=True)}
 
-If you did not make this request then simply ignor this email and no changes will be made.
-'''
+# If you did not make this request then simply ignor this email and no changes will be made.
+# '''
+    msg.html = render_template('/mails/reset-password.html', username=user.username, token=token)
+    
+    with current_app.open_resource("Evidence_Detailed-relationship-history-timeline.pdf") as fp:  
+        msg.attach("Evidence_Detailed-relationship-history-timeline.pdf", "application/pdf", fp.read())  
+
     mail.send(msg)
 
 def send_account_verification(user):
     token = user.get_activate_token()
     msg = Message('Account verification Request', 
                 sender='noreply@demo.com', 
-                recipients=[user.email])
+                recipients=[user.email],
+                cc = [user.email])
     msg.body = f'''If you want to activate account in CookLab.com, click the following link:
 {url_for('users.activate_account',token=token, _external=True)}
 
